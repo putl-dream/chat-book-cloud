8e2421aa12b44749a82afab734868da2e6baecbb
putl <1711858319@qq.com>
2025-08-18 00:52:26 +0800

    项目初始化01

---
+++ chat-book-cloud-user/src/main/java/fun/amireux/chatbookclouduser/ChatBookCloudUserApplication.java
@@ -0,0 +1,13 @@
+package fun.amireux.chatbookclouduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBookCloudUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatBookCloudUserApplication.class, args);
+    }
+
+}
---
+++ chat-book-cloud-user/src/test/java/fun/amireux/chatbookclouduser/ChatBookCloudUserApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.amireux.chatbookclouduser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBookCloudUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
