81af016fbef8a1ed1076fbdb154fa43d4b9f6f15
putl <1711858319@qq.com>
2025-08-19 23:28:16 +0800

    项目初始化08 解决无法转发问题

--- chat-book-cloud-gateway/src/main/java/fun/amireux/chat/book/gateway/GatewayServiceApplication.java
+++ chat-book-cloud-gateway/src/main/java/fun/amireux/chat/book/gateway/GatewayServiceApplication.java
@@ -1,12 +1,7 @@
 package fun.amireux.chat.book.gateway;
 
-import fun.amireux.chat.book.framework.security.config.SecurityConfig;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Import;
 
-@EnableDiscoveryClient
-@Import(SecurityConfig.class)
 @SpringBootApplication
 public class GatewayServiceApplication {
     public static void main(String[] args) {
