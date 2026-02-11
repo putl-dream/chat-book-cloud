31716b0f3951c8f44710f0a46d39af7d04f55976
putl <1711858319@qq.com>
2026-02-11 15:37:07 +0800

    更新测试配置以使用模拟bean并调整数据库初始化设置
    - 在AmqpTest类中将RabbitTemplate从@Autowired替换为@MockBean
    - 将application.yml中的sql init模式从always更改为never
    - 为article服务的测试配置添加server port: 0设置
    - 为user服务的测试配置添加server port: 0设置

--- chat-book-cloud-article/src/test/java/com/putl/articleservice/mq/AmqpTest.java
+++ chat-book-cloud-article/src/test/java/com/putl/articleservice/mq/AmqpTest.java
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 public class AmqpTest {
 
-    @Autowired
+    @MockBean
     private RabbitTemplate rabbitTemplate;
 
     @Test
