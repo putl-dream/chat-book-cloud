f4b9ca2aa51e45d47d6087b488156cded53803c4
putl <1711858319@qq.com>
2026-03-21 12:05:40 +0800

    重构Redis配置实现统一框架
    - 将各模块独立的RedisConfig移至统一的chat-book-cloud-redis框架
    - 更新META-INF/spring配置文件使用新的RedisConfig路径
    - 修改所有业务模块pom.xml依赖从spring-boot-starter-data-redis改为chat-book-cloud-redis
    - 在chat-book-cloud-redis中添加jackson-datatype-jsr310依赖支持时间类型序列化
    - 采用GenericJackson2JsonRedisSerializer替代Jackson2JsonRedisSerializer
    - 添加条件注解@ConditionalOnClass、@ConditionalOnBean和@ConditionalOnMissingBean
    - 移除旧的RedisProperties配置类并整合到AutoConfiguration中

--- chat-book-cloud-framework/chat-book-cloud-redis/src/main/java/fun/amireux/chat/book/framework/redis/config/RedisConfig.java
+++ chat-book-cloud-framework/chat-book-cloud-redis/src/main/java/fun/amireux/chat/book/framework/redis/config/RedisConfig.java
@@ -1,61 +1,52 @@
 package fun.amireux.chat.book.framework.redis.config;
 
-
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import fun.amireux.chat.book.framework.redis.pojo.RedisProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisOperations;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
-@Configuration
-@EnableConfigurationProperties(RedisProperties.class)
+@AutoConfiguration(after = RedisAutoConfiguration.class)
+@ConditionalOnClass(RedisOperations.class)
+@ConditionalOnBean(RedisConnectionFactory.class)
 public class RedisConfig {
 
-    @Bean
-    public LettuceConnectionFactory redisConnectionFactory(RedisProperties redisProperties) {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setHostName(redisProperties.getHost());
-        config.setPort(redisProperties.getPort());
-        config.setPassword(redisProperties.getPassword());
-        config.setDatabase(redisProperties.getDatabase());
-        return new LettuceConnectionFactory(config);
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+    @Bean("objectRedisTemplate")
+    @Primary
+    @ConditionalOnMissingBean(name = "objectRedisTemplate")
+    public RedisTemplate<String, Object> objectRedisTemplate(RedisConnectionFactory connectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(factory);
+        template.setConnectionFactory(connectionFactory);
 
-        // 定义Jackson2JsonRedisSerializer序列化对象
-        ObjectMapper om = new ObjectMapper();
-        // 指定要序列化的域，field,get和set,以及修饰符范围，ANY是都有包括private和public
-        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        // 设置属性命名策略为蛇形命名法（可选）
-        om.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        StringRedisSerializer keySerializer = new StringRedisSerializer();
+        GenericJackson2JsonRedisSerializer valueSerializer =
+                new GenericJackson2JsonRedisSerializer(createRedisObjectMapper());
 
-        // 使用构造函数传入ObjectMapper
-        Jackson2JsonRedisSerializer<Object> jacksonSeial = new Jackson2JsonRedisSerializer<>(om, Object.class);
-
-        StringRedisSerializer stringSerial = new StringRedisSerializer();
-        // redis key 序列化方式使用stringSerial
-        template.setKeySerializer(stringSerial);
-        // redis value 序列化方式使用jackson
-        template.setValueSerializer(jacksonSeial);
-        // redis hash key 序列化方式使用stringSerial
-        template.setHashKeySerializer(stringSerial);
-        // redis hash value 序列化方式使用jackson
-        template.setHashValueSerializer(jacksonSeial);
-
+        template.setKeySerializer(keySerializer);
+        template.setHashKeySerializer(keySerializer);
+        template.setValueSerializer(valueSerializer);
+        template.setHashValueSerializer(valueSerializer);
         template.afterPropertiesSet();
         return template;
+    }
+
+    private ObjectMapper createRedisObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
     }
 }
